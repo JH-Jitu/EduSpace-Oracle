84225f281ec7e2309e8c14a3cecb2e0d64cd32b6
JH-Jitu <jitu000166@gmail.com>
2023-03-18 21:22:32 +0600

    connected oracle database

--- Controller/db_connect.php
+++ Controller/db_connect.php
@@ -6,6 +6,20 @@ $username = "root";
 $password = "";
 $dbname = "login_system";
 
+// For Oracle
+$conn_str = "localhost:1521/ORCL";
+$username2 = "eduspace";
+$password2 = "eduspace23";
+$dbname2 = "login_system";
+
+$conn2 = oci_connect($username2, $password2, $conn_str);
+
+if (!$conn2) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+} else {
+    echo "Connected with Oracle";
+}
 // create a new database connection
 // $conn = new mysqli($servername, $username, $password, $dbname);
 $conn = mysqli_connect($servername, $username, $password, $dbname);
